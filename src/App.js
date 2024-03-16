9ec4a1c79a47596d02ce1f8475ea9e5b7bbfc51a
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-16 21:09:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import data from './Data';
+import Tours from './components/Tours';
 import './App.css';
+import React,{ useState } from 'react';
 
 function App() {
+  const [tours, setTours] =useState(data);
+  function removeTour(id)
+  {
+    const newTours = tours.filter(tour=>tour.id!==id);
+    setTours(newTours);
+  }
+
+  if(tours.length===0){
+    return (
+      <div className = "refresh">
+        <h2>No tours left</h2>
+        <button className = "btn-white" onClick = {()=>setTours(data)}>Refresh</button>
+
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Tours tours={tours} removeTour= {removeTour}></Tours>
+
     </div>
   );
 }
